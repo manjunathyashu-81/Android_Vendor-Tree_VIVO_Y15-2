8d06c04ca2675c644f6ebc9902fe2b9a93eb2320
manjunath yashu <35634330+manjunathyashu-81@users.noreply.github.com>
2018-01-21 15:49:03 +0530

    Create Y15-vendor.mk

---
+++ Y15-vendor.mk
@@ -0,0 +1,9 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/VIVO/Y15/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/VIVO/Y15/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/VIVO/Y15/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/VIVO/Y15/proprietary/vendor,system/vendor)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/VIVO/Y15/proprietary/xbin,system/xbin)
